a4ee94fdfa1f24c7643b394a38298c4d3bb4d69c
olofs22 <olof.business.svensson@gmail.com>
2025-09-16 19:53:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP_ovning
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ väg.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace OOP_ovning
+{
+   public class Väg
+    {
+        public string Material;
+        public string Längd;
+        public string Bredd;
+    }
+}
